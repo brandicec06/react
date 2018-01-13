d82834b9983ae15dc25d3e7901ba94f0422bc310
brandicec06 <specv5@gmail.com>
2018-01-13 12:53:01 -0500

    tesla example start

---
+++ ex2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/header/Header.js'
+
+import {Form, FormControl, Button} from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
---
+++ ex2/src/containers/TeslaBattery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TeslaBattery.css';
+import TeslaNotice from '../components/TeslaNotice/TeslanNotice.js';
+
+class TeslaBattery extends React.Component {
+   
+    render(){
+        return(
+            <form className="tesla-battery">
+                <h1> Range Per Charge </h1>
+                <TeslaNotice />
+            </form>
+           
+        )
+    }
+}
+
+export default TeslaBattery;
